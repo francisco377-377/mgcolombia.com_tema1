7ff90d98fb2d706fed99ab4bd71f0cc2262bf66e
francisco377-377 <francisco.vanegas1991@gmail.com>
2026-04-27 23:59:07 -0500

    feat: actualizar número de WhatsApp para Yopal

--- generate_cities.js
+++ generate_cities.js
@@ -43,7 +43,7 @@ const cities = [
   { name: "Túquerres", slug: "tuquerres", delivery: "5 a 6 días hábiles" },
   { name: "Valledupar", slug: "valledupar", delivery: "5 a 6 días hábiles" },
   { name: "Villavicencio", slug: "villavicencio", delivery: "5 a 6 días hábiles", waNumber: "573503189981", waDisplay: "350 318 99 81" },
-  { name: "Yopal", slug: "yopal", delivery: "5 a 6 días hábiles" }
+  { name: "Yopal", slug: "yopal", delivery: "5 a 6 días hábiles", waNumber: "573503189981", waDisplay: "350 318 99 81" }
 ];
 
 cities.forEach(city => {
